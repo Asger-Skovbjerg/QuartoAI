96867ee99c3a8028631e6b653a933ef9af5ad78a
AnnikeKristensen <AnnikeKristensen@gmail.com>
2022-01-18 11:05:04 +0100

    started GUI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from quarto import Quarto
 import numpy as np
+import pygame
 
 q = Quarto()
 q.init_pieces()
@@ -12,3 +13,36 @@ for x in range(4):
 for piece, position in zip(q.pieces, positions):
     q.place_piece(piece, position)
 print(q.board)
+
+def draw_grid(surface,matrix,size):
+    """Draws a grid on surface given"""
+    #TODO: for every matrix cell, check status and put either empty cell or image of given token
+
+
+game_running = True
+width = 800
+height = 500
+pygame.init()
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Quarto - Asger & Annike")
+clock = pygame.time.Clock()
+surface = pygame.Surface((width,height))
+surface.fill('White')
+
+
+while game_running:
+    for event in pygame.event.get(): #getting user input
+        if event.type == pygame.QUIT: #if closing the window, quit.
+            """#pygame.quit quits pygame we initalized in pygame.init()
+             and pygame.display.update() will then throw a "video system not initialized" error. 
+            A solution to this is importing from sys import exit and then call exit()
+            However exit() will stop any code still going on, and we might still want to be able to run the AI 
+            even when we have closed the window. """
+            pygame.quit()
+            game_running == False
+
+    screen.blit(surface,(0,0))
+    pygame.display.update()
+    clock.tick(60) #while loop should not run faster than 60 times pr second
+
+
